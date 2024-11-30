895890961890137d07fae08955be030f46844fdc
mayurshinde <mayurss9449@gmail.com>
2024-11-30 22:24:01 +0530

    clone testing 1

--- react-testing-git/src/App.jsx
+++ react-testing-git/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -14,6 +15,7 @@ function App() {
         
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, beatae doloribus, harum soluta et fugit natus placeat quo voluptatibus ipsa quam. Incidunt necessitatibus maxime numquam sit voluptates! Iste optio atque placeat natus, laboriosam eaque error sapiente maiores odit perferendis, neque dolore id iusto doloremque modi. Atque fuga, explicabo similique, quidem quos dolores, deleniti nobis velit excepturi nisi aspernatur exercitationem dolorum illo quae ipsam? Deserunt, corporis! Similique dolorum itaque nisi ad quasi, vero dignissimos veniam eius consequuntur quidem alias optio, iure molestias in minus atque non, minima illo assumenda! Fugiat, natus! Illum quo eum, accusantium animi eveniet aspernatur dicta quisquam incidunt.</p>
       </div>
+      <Header />
     </>
   )
 }
