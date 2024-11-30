50346dd39f11fb2eb0cd7992122fcfeaeb2ba0ec
mayurshinde <mayurss9449@gmail.com>
2024-11-30 23:01:01 +0530

    branch 3 commit

--- react-testing-git/src/Home.jsx
+++ react-testing-git/src/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Home page from branch 
+      <h1>Home page from branch</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo explicabo odio iure vel odit corrupti repellendus alias non atque expedita, quos accusantium officiis labore quod totam magni tempore in aspernatur sunt minus nobis at dicta optio? Molestias, ipsa vitae eos sunt soluta in eligendi inventore, libero repellat praesentium aut, maxime quo nesciunt id cum tenetur quidem quaerat. Debitis eius voluptates hic tempore laboriosam consequuntur repellat temporibus ullam in rerum distinctio quae, id praesentium nulla. Iste unde dolor tempore perferendis eius, pariatur temporibus, sunt adipisci, sed saepe ad culpa vero reiciendis. Repellat dignissimos, iure eveniet velit rem corrupti expedita in illo.</p>
     </div>
   )
 }
